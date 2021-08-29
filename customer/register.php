456275b545c6f95466dd840f237704e5d76c4a45
thienv29 <thienv29@gmail.com>
2021-08-29 22:54:36 +0700

    ss

---
+++ customer/register.php
@@ -0,0 +1,46 @@
+
+<?php
+  include('./saveRegister.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/booststrap.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="register">
+    <form class=" p-4" action="register.php" method="POST">
+      <h3>Register</h3>
+      
+      <div class="form-group">
+        <label for="exampleDropdownFormPassword2">Full name</label>
+        <input type="text" name="name" class="form-control" id="exampleDropdownFormPassword2" placeholder=" Your full name">
+      </div>
+      <div class="form-group">
+        <label for="exampleDropdownFormEmail2">Password</label>
+        <input type="text" name="password" class="form-control" id="exampleDropdownFormEmail2" placeholder="Enter Your's password">
+      </div>
+      <div class="form-group">
+        <label for="exampleDropdownFormEmail2">Address</label>
+        <input type="text"  name="address" class="form-control" id="exampleDropdownFormEmail2" placeholder="Enter Your's Address">
+      </div>
+      <div class="form-group">
+        <label for="exampleDropdownFormEmail2">City</label>
+        <input type="text" name="city" class="form-control" id="exampleDropdownFormEmail2" placeholder="Enter Your's City">
+      </div>
+
+      
+
+      <button type="submit" class="btn login-btn btn-primary">Register</button>
+    </form>
+  </div>
+</body>
+
+</html>
